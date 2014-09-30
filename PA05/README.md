ad29d8ead99e680b9df6492f1885c04e4eaea15b
connorkirby32 <ckirby@nevada.unr.edu>
2014-09-30 15:17:28 -0700

    PA05

---
+++ PA05/README.md
@@ -0,0 +1,8 @@
+Assimp example model loader.
+
+Enter the desired file to be loaded as a command line argument when running executable. 
+
+Current OBJ  in bin is gourd. 
+
+Coding ideas came from: http://www.lighthouse3d.com/cg-topics/code-samples/importing-3d-models-with-assimp/
+
---
+++ PA05/src/shaderLoader.h
@@ -0,0 +1,32 @@
+#include <fstream>
+#include<string>
+
+
+
+
+using namespace std;
+
+
+
+class shaderLoader{
+    public:
+        char * load(string fileName){
+        
+        ifstream fin(fileName.c_str());
+        
+        //Find File Length
+        fin.seekg(0,std::ios::end);
+        streampos length = fin.tellg();
+        fin.seekg(0,std::ios::beg);
+        
+        //Allocate and copy to char pointer
+        buffer = new char [length];
+        fin.read(&buffer[0], length);
+        return buffer;
+   }
+    
+    private:
+    char * buffer;
+};
+
+
